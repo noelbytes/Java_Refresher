2790ed869dfe2e259ce3b0f7cb07fd801ce4d729
noelbytes <allan34291@gmail.com>
2024-01-16 23:46:32 +0530

    Added methods for display, push, pop, and peek

--- 3. Algorithms_and_Data_Structures_in_Java/1. Introduction - Algorithms_and_Data_Structures/3. Stacks_and_Queues/src/TheStack.java
+++ 3. Algorithms_and_Data_Structures_in_Java/1. Introduction - Algorithms_and_Data_Structures/3. Stacks_and_Queues/src/TheStack.java
@@ -18,6 +18,81 @@ public class TheStack {
 	private int stackSize;
 	private int topOfStack = -1; // the stack is initially empty
 
+	public void push(String input) {
+		if (topOfStack + 1 < stackSize) {
+			topOfStack++;
+
+			stackArray[topOfStack] = input;
+		} else {
+			System.out.println("Sorry, but the stack is full");
+		}
+
+		displayTheStack();
+
+		System.out.println("PUSH " + input + " was added to the stack");
+	}
+
+	public String pop() {
+		if (topOfStack >= 0) {
+			displayTheStack();
+
+			System.out.println("POP " + stackArray[topOfStack] + " was removed from the stack\n");
+
+			stackArray[topOfStack] = "-1"; // Done for display purposes. In memory, when you delete an item, it is still
+											// going to be there, however, it will have the value of -1 instead of the
+											// original value
+			
+			return stackArray[topOfStack--];
+		} else {
+			displayTheStack();
+			
+			System.out.println("Sorry, but the stack is empty");
+			
+			return "-1";
+		}
+	}
+	
+	public String peek() {
+		// This method will return what's on top of the stack, but won't remove it
+		displayTheStack();
+		
+		System.out.println("PEEK " + stackArray[topOfStack] + " is at the top of the stack\n");
+		
+		return stackArray[topOfStack];
+	}
+
+	public void displayTheStack() {
+		for (int iteration = 0; iteration < 61; iteration++)
+			System.out.print("-");
+
+		System.out.println();
+
+		for (int number = 0; number < stackSize; number++) {
+			System.out.format("| %2s " + " ", number);
+		}
+
+		System.out.println("|");
+
+		for (int iteration = 0; iteration < 61; iteration++)
+			System.out.print("-");
+		
+		System.out.println();
+		
+		for (int index = 0; index < stackSize; index++) {
+			if (stackArray[index].equals("-1"))
+				System.out.print("|     ");
+			else
+				System.out.print(String.format("| %2s " + " ", stackArray[index]));
+		}
+		
+		System.out.println("|");
+		
+		for (int iteration = 0; iteration < 61; iteration++)
+			System.out.print("-");
+		
+		System.out.println();
+	}
+
 	TheStack(int size) {
 		stackSize = size;
 		stackArray = new String[size];
@@ -25,6 +100,9 @@ public class TheStack {
 	}
 
 	public static void main(String[] args) {
-
+		
+		TheStack theStack = new TheStack(10);
+		
+		theStack.push("10");
 	}
 }
