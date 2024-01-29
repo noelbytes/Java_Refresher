42e8c5b7853ee7094005e8a55a5edcd9c99050e9
noelbytes <allan34291@gmail.com>
2024-01-29 23:36:38 +0530

    Added the drawSquares(int), calculateSquaresToPrint(int) and the calculateTraingularNumber(int) method

--- 3. Algorithms_and_Data_Structures_in_Java/2. Java_Recursion/Recursion/src/Recursion.java
+++ 3. Algorithms_and_Data_Structures_in_Java/2. Java_Recursion/Recursion/src/Recursion.java
@@ -10,10 +10,51 @@
  * 		} else {
  * 			return (number + getTraingularNumber(number - 1));
  * }
-} */
+} 
+*/
 
 public class Recursion {
 	public static void main(String[] args) {
+		Recursion recursionTool = new Recursion();
 		
+		recursionTool.calculateSquaresToPrint(6);
+	}
+	
+	// USED TO DEMONSTRATE TRIANGULAR NUMBERS -----------------------
+	
+	// Draws the number of squares that are passed in horizontally
+	
+	public void drawSquares(int howManySquares) {
+		for (int iteration = 0; iteration < howManySquares; iteration++) {
+			System.out.print(" --  ");
+		}
+		
+		System.out.println();
+		
+		for (int iteration = 0; iteration < howManySquares; iteration++) {
+			System.out.print("|" + howManySquares + " | ");
+		}
+		
+		System.out.println();
+		
+		for (int iteration = 0; iteration < howManySquares; iteration++) {
+			System.out.print(" --  ");
+		}
+		
+		System.out.println("\n");
+	}
+	
+	// Outputs the number of squares to print to represent a triangle
+	public void calculateSquaresToPrint(int number) {
+		for (int index1 = 1; index1 <= number; index1++) {
+			for (int index2 = 1; index2 < index1; index2++) {
+				drawSquares(index2);
+			}
+			System.out.println("Triangular Number : " + calculateTraingularNumber(index1));
+		}
+	}
+	
+	public double calculateTraingularNumber(int number) {
+		return .5 * number * (1 + number);
 	}
 }
