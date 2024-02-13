cda00e3150d0143127c6f4bac87db4ede7a3039d
noelbytes <allan34291@gmail.com>
2024-02-13 11:03:51 +0530

    Added a boilerplate for the Shell Sort algorithm

---
+++ 3. Algorithms_and_Data_Structures_in_Java/3. Sorting/1. Java_Shell_Sort/src/ShellSort.java
@@ -0,0 +1,9 @@
+/*The Shell Sort partially sorts the array by sorting elements that are a certain 
+ * number of spaces apart called the interval
+ * The index to the left will be referred to as the inner index, and h -  will be representing the interval, 
+ * or number of spaces apart, and this is also known as the outer part of the array when using the shell sort. 
+ * The performance of the shell sort algorithm improves when compared to the insertion sort algorithm, 
+ * since we eliminate the need to shift many indexes in the array. */
+public class ShellSort {
+
+}
