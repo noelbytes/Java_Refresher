eef9f0437f4f9e2098523ffcf6dead6bb72c113d
noelbytes <allan34291@gmail.com>
2024-03-20 22:43:04 +0530

    Formatted spaces in the recursion source file

--- 3. Algorithms_and_Data_Structures_in_Java/2. Java_Recursion/Recursion/src/Recursion.java
+++ 3. Algorithms_and_Data_Structures_in_Java/2. Java_Recursion/Recursion/src/Recursion.java
@@ -19,8 +19,7 @@ public class Recursion {
 
 //		recursionTool.calculateSquaresToPrint(6);
 
-		System.out.println("Triangular Number : " + recursionTool.getTriangularNumber(6)); // 21 is the triangular
-																							// number for 6
+		System.out.println("Triangular Number : " + recursionTool.getTriangularNumber(6)); // 21 is the triangular number for 6
 		System.out.println("-------------------------------------------");
 
 		System.out.println("Triangular Number : (Using recursion) = " + recursionTool.getTriangularNumberByRecursion(6));
